a4cdc8c87b0d18de973fd2e6e75f870d8139170f
Jennifer Neuman <jennifer.neuman1@pcc.edu>
2016-10-25 15:03:55 -0700

    added main method to print hello world

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -4,4 +4,8 @@ package helloworld;
  * Created by Jenny on 10/25/2016.
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
 }
