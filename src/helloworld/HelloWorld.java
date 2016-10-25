6aa4e4ea3c4e4881021e4301ea648aa7fb04d0bc
Jennifer Neuman <jennifer.neuman1@pcc.edu>
2016-10-25 15:21:51 -0700

    added line to main method to print date

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,11 +1,17 @@
 package helloworld;
 
+import java.util.Date;
+
 /**
- * Created by Jenny on 10/25/2016.
+ * CIS233J, section 40706, Fall 2016
+ * Assignment 4
+ * @author Jennifer Neuman
+ * @version 10/25/2016
  */
 public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        System.out.println(new Date());
     }
 }
